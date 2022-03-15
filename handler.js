bf2ac29c43336c63381eb9a89758aad9a939a42c
Valentin <valentin@sauer-medientechnik.de>
2022-03-15 13:08:07 +0100

    make it work

--- handler.js
+++ handler.js
@@ -1,15 +1,56 @@
 "use strict";
 
-module.exports.hello = async (event) => {
+var WetterheldClient = require('./lib/wetterheldClient').WetterheldClient;
+
+console.log(process.env.WETTERHELD_URL);
+
+const serverUrl = process.env.WETTERHELD_URL;
+const laravelToken = process.env.WETTERHELD_LARAVEL_TOKEN;
+
+module.exports.price = async (event) => {
+
+  if(!serverUrl || !laravelToken){
+    return {
+      statusCode: 500,
+      body: JSON.stringify(
+        "Missing env vars",
+        null,
+        2
+      ),
+    };
+  }
+  
+  const options = {
+    serverUrl,
+    laravelToken
+  }
+  var client = WetterheldClient(options);
+
+  try {
+    await client.all.selectProduct();
+    await client.all.getWeatherSources();
+    await client.all.determineStation();
+    await client.all.findStation();
+    var finalResponse = await client.all.orderPrice();
+
+  }catch(e){
+    return {
+      statusCode: 500,
+      body: JSON.stringify(
+        "Wetterheld API Error: " + e.errorMessage,
+        null,
+        2
+      ),
+    };
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
-      },
+      finalResponse,
       null,
       2
     ),
   };
+  
 };
